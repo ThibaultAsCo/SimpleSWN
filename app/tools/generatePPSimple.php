38c4711214fe8e9c4ddef6993f002329dbebf599
LafQuentin <quentin.laffeach@gmail.com>
2016-06-23 09:10:12 +0200

    addGeneratePP

---
+++ app/tools/generatePPSimple.php
@@ -0,0 +1,98 @@
+<?php
+//////////////////////////////////////
+//            Mots clefs            //
+//             (array)              //
+//////////////////////////////////////
+
+$keyWords = array('L\'ÉTÉ-de', 'B', 'C', 'D');
+
+//////////////////////////////////////
+//           Localisation           //
+//             (array)              //
+//////////////////////////////////////
+
+$local = array('L\'ÉTANG-ici', '2', '3', '4', '5');
+
+//////////////////////////////////////
+//           Info Societe           //
+//////////////////////////////////////
+
+$societe = 'Name';
+$societeLoc = 'Ville';
+
+//////////////////////////////////////
+//            Description           //
+//                                  //
+//////////////////////////////////////
+
+
+$descMini = 'l\'été il fait chaud';
+
+//////////////////////////////////////
+
+echo'<ul>';
+
+for ($i=0; $i<count($keyWords); $i++) {
+    for ($ii=0; $ii<count($local); $ii++){
+
+        $jeanMoricePimpon = array('\'', 'é', 'É', 'È', 'è', 'ç', 'Ç', 'À', 'à', 'â', 'ê', 'î', 'ô', 'û', 'Â', 'Ô', 'Î', 'Ê', 'Û');
+        $jeanMoriceChiuauaForIsa = array('-', 'e', 'E', 'E', 'e', 'c', 'C', 'A', 'a', 'a', 'e', 'i', 'o', 'u', 'A', 'O', 'I', 'E', 'U');
+
+
+
+        $LOL = str_replace('-', ' ', $keyWords[$i]);
+        $LOL .= ' '.ucfirst(str_replace('-', ' ', $local[$ii]));
+
+//////////////////////////////////////
+//               Texte              //
+//              (array)             //
+//    (<strong>'.$LOL.'</strong>)   //
+//////////////////////////////////////
+
+
+        $textOut = array(
+            '(<strong>'.$LOL.'</strong>)'
+        ,
+            '(<strong>'.$LOL.'</strong>)'
+        ,
+            '(<strong>'.$LOL.'</strong>)'
+        ,
+            '(<strong>'.$LOL.'</strong>)');
+
+
+
+//////////////////////////////////////
+//           Template               //
+//////////////////////////////////////
+
+        $template = '
+            <?php
+                $title = "'.ucfirst($LOL).'";
+                $keywords = "'.str_replace('-', ' ', $keyWords[$i]).', '.str_replace('-', ' ', $local[$ii]).'";
+                $description = "'.$societe.' - '.ucfirst($LOL).' - '.$descMini.' '.str_replace('-', ' ', $local[$ii]).'";
+            ?>';
+
+        $template .='';
+
+        shuffle($textOut);
+        foreach($textOut as $finalOut)
+            $template .= '<p>'.$finalOut.'</p>';
+
+        $template .= '';
+
+//////////////////////////////////////
+//        Génération Pages          //
+//////////////////////////////////////
+
+
+        $open = fopen(strtolower(str_replace($jeanMoricePimpon, $jeanMoriceChiuauaForIsa, $keyWords[$i])).'-'.strtolower(str_replace($jeanMoricePimpon, $jeanMoriceChiuauaForIsa, $local[$ii])).".php", "w");
+        fwrite($open, $template);
+        fclose($open);
+
+        $template .= '';
+
+        echo '<li>Fichier <strong>'.$keyWords[$i].'-'.$local[$ii].'</strong>.php cr&eacute;&eacute;</li>';
+    }
+}
+echo'</ul><br>';
+
