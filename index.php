cbee08f6796cce0005f5576aedbb94e59f2d2fba
ThibaultAsCo <thibault.d@asconconsulting.fr>
2016-07-12 10:17:57 +0200

    Add H1

--- index.php
+++ index.php
@@ -7,28 +7,30 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
         <meta name="description" content="<?= $description ?>">
         <meta name="author" content="<?= $configSite->authorsite; ?>">
         <meta name="keywords" content="<?= $keywords ?>">
         <title><?= $title ?> - <?= $configSite->sitename; ?></title>
-    
+
         <link rel="icon" href="">
         <!--<link rel="stylesheet" href="assets/css/pp.css" />-->
-    
+
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    
+
     <body>
-    
+
+      <h1>Chaussette !!</h1>
+
         <?= $data; ?>
 
         <?php $pp = new PokerPage(); ?>
         <?= $pp->pokerFoo(); ?>
-    
+
     </body>
 </html>
