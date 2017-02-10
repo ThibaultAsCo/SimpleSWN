e79518fdae97a09c8d0fc2a3c3ebc192801a6673
ThibaultAsCo <thibault.d@asconconsulting.fr>
2017-02-10 13:48:36 +0100

    404 - Img

---
+++ page/404.php
@@ -0,0 +1,21 @@
+<?php
+    $title = '404';
+    $description = '';
+    $keywords = '';
+?>
+
+
+
+
+<section>
+  <div class="container">
+
+    <div class="col-xs-12 col-md-6">
+      <p>
+        Oups ! Il semblerait que la page n'existe plus :/
+      </p>
+
+    </div>
+
+  </div>
+</section>
