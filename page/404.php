b75af15acc801130b7506d6a5506b4c4f1adfe5c
ThibaultAsCo <thibault.d@asconconsulting.fr>
2017-03-06 09:37:40 +0100

    404 / bootstrap.min
    404 / bootstrap.min

--- page/404.php
+++ page/404.php
@@ -4,15 +4,14 @@
     $keywords = '';
 ?>
 
-
-
-
-<section>
+<section class="pageErreur">
   <div class="container">
-
-    <div class="col-xs-12 col-md-6">
-      <p>
-        Oups ! Il semblerait que la page n'existe plus :/
+    <div class="col-xs-12 text-center">
+      <h1>404</h1>
+      <p>Oups ! Nous ne parvenons pas à trouver la page que vous cherchez.
+        <a href="accueil.html" class="btn btn-primary retour404" role="button">
+          Retour à l'accueil
+        </a>
       </p>
 
     </div>
