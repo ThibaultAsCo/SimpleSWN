ad98a16f1d0c9602466e457f6d2c3b72b7de9ca3
ThibaultAsCo <thibault.d@asconconsulting.fr>
2017-03-30 09:37:56 +0200

    MAJ LESS Menu

--- page/mentions-legales.php
+++ page/mentions-legales.php
@@ -48,9 +48,7 @@
       </div>
 
 
-    </div> <!-- Fin row -->
 
-    <div class="row">
       <div class="container">
 
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> <!--Colonne gauche-->
