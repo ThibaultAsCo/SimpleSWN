6add3013692d7af6f13bae992f3789e5da3f6fe4
ThibaultAsCo <thibault.d@ascoconsulting.com>
2017-05-09 08:26:39 +0200

    Htacces cache navigateur

--- assets/js/gestionCarousel.js
+++ assets/js/gestionCarousel.js
@@ -1,9 +1,16 @@
 $(function() {
 
+    $(".diaporama").owlCarousel({
+      items:1,
+      autoplay:true,
+      autoHeight: false,
+    });
+
+
     $(".realisations").owlCarousel({
       items:5,
       autoplay:true,
-      dots:true
+      dots:true,
     });
 
 
--- assets/template/diaporama.php
+++ assets/template/diaporama.php
@@ -1,7 +1,38 @@
+<?php
+  $dir = './assets/img/template/';
 
+  $valide_extensions = array('jpg', 'jpeg', 'gif', 'png');
+  $files=array();
 
-<div id="owl-example" class="owl-carousel">
-  <div><img src="assets/img/template/02.jpg" class="img-responsive"></div>
-  <div><img src="assets/img/template/01.jpg" class="img-responsive"></div>
-  <div><img src="assets/img/template/03.jpg" class="img-responsive"></div>
+  $Ressource = opendir($dir);
+  while($fichier = readdir($Ressource))
+  {
+    $berk = array('.', '..');
+
+    $test_Fichier = $dir.$fichier;
+
+    if(!in_array($fichier, $berk) && !is_dir($test_Fichier))
+    {
+      $ext = pathinfo($fichier,  PATHINFO_EXTENSION);
+
+      if(in_array($ext, $valide_extensions))
+      {
+        $files[]=$test_Fichier;
+      }
+    }
+  }
+  sort($files);
+?>
+
+<div class="owl-carousel diaporama">
+  <?php
+    for ($i = 0;$i < count($files) ; $i++) {
+      $test_Fichier = $files[$i];
+      echo '
+        <div class="item">
+          <img src="'.$test_Fichier.'" title="ICI le titre de mon image" class="img-responsive" />
+        </div>
+      ';
+    }
+  ?>
 </div>
